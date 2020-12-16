10f30a4fb456765bd535890baad5de9e4cce47c0
Francesca Nemati <FrancescaNemati@Francescas-MBP.lan>
2020-12-15 19:10:57 -0500

    my tester - simple tests for Sorts seem to work

---
+++ TestSorts.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class TestSorts {
+
+  public static void main(String[] args) {
+    int[] mantest1 = new int[5];
+    mantest1[0] = 5;
+    mantest1[1] = 1;
+    mantest1[2] = 12;
+    mantest1[3] = -5;
+    mantest1[4] = 16;
+    Sorts.bubbleSort(mantest1);
+    System.out.println();
+
+    int[] mantest2 = new int[5];
+    mantest2[0] = 2;
+    mantest2[1] = 3;
+    mantest2[2] = 4;
+    mantest2[3] = 5;
+    mantest2[4] = 1;
+    Sorts.bubbleSort(mantest2);
+    System.out.println();
+
+    int[] mantest3 = new int[6];
+    mantest3[0] = 6;
+    mantest3[1] = 1;
+    mantest3[2] = 2;
+    mantest3[3] = 3;
+    mantest3[4] = 4;
+    mantest3[5] = 5;
+    Sorts.bubbleSort(mantest3);
+  }
+
+}
